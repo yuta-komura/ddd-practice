0cf48e169ee975b435a643856eaea9cca549903d
yuta-komura <esfgshdfsd@gmail.com>
2023-01-02 17:07:27 +0900

    [update] プロジェクト名の修正 demoからddd-practiceに変更したため

--- src/test/kotlin/com/yutakomura/ApplicationTests.kt
+++ src/test/kotlin/com/yutakomura/ApplicationTests.kt
@@ -1,13 +1,13 @@
-package demo
+package com.yutakomura
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class DemoApplicationTests {
+class ApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Test
+    fun contextLoads() {
+    }
 
 }
