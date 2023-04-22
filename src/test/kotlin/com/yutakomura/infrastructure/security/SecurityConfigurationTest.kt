bd90ff909f0d3244e9341e8a1588e23c30e91a1b
yuta-komura <esfgshdfsd@gmail.com>
2023-04-22 22:38:15 +0900

    test

--- src/test/kotlin/com/yutakomura/infrastructure/security/SecurityConfigurationTest.kt
+++ src/test/kotlin/com/yutakomura/infrastructure/security/SecurityConfigurationTest.kt
@@ -3,12 +3,11 @@ package com.yutakomura.infrastructure.security
 import com.auth0.jwt.interfaces.DecodedJWT
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.yutakomura.domain.user.Email
+import com.yutakomura.domain.user.EncodedPassword
+import com.yutakomura.domain.user.Password
 import com.yutakomura.domain.user.UserRepository
 import com.yutakomura.infrastructure.security.JWTProvider.Companion.X_AUTH_TOKEN
-import com.yutakomura.usecase.user.signup.SignupInputData
 import com.yutakomura.usecase.user.signup.SignupUseCase
-import io.mockk.every
-import io.mockk.mockk
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -18,7 +17,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
-import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.post
 import java.nio.file.Paths
@@ -50,13 +48,13 @@ class SecurityConfigurationTest {
 
     @BeforeEach
     fun setup() {
-        val email = "aaa@gmail.com"
-        val password = "aaaa"
-        userRepository.deleteByEmail(Email(email))
-        val inputData = SignupInputData(email, password)
-        val passwordEncoderMock = mockk<PasswordEncoder>()
-        every { passwordEncoderMock.encode(password) } returns "{bcrypt}\$2a\$10\$275XYyZ6IOE2p0Jxw4lY7.UeZo9oZRJJS9qtD/DJ3pZa39gBeZ4Y."
-        signup.handle(inputData)
+        val email = Email("aaa@gmail.com")
+        userRepository.deleteByEmail(email)
+
+        val password = Password("aaaa")
+        val encodedPassword = EncodedPassword.from(password)
+        userRepository.insert(email, encodedPassword)
+//        every { passwordEncoderMock.encode(password) } returns "{bcrypt}\$2a\$10\$275XYyZ6IOE2p0Jxw4lY7.UeZo9oZRJJS9qtD/DJ3pZa39gBeZ4Y."
     }
 
     @Test
